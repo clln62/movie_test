e87ba335d2b8ae7ea25683d6473f9639abb3f1da
Collin Fairlie <collin.fairlie@gmail.com>
2020-12-04 13:30:23 -0800

    Corrected typo for API key.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const axios = require('axios');
 
-const MOVIE_API_BASE_URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=`;
+const MOVIE_API_BASE_URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&language=en-US&query=`;
 const keyword = 'Titanic';
 
 axios.get(`${MOVIE_API_BASE_URL}${keyword}`)
